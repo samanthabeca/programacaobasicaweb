392361773df73ea2f164219aadbf5cb4f93d0498
samanthabeca <61758809+samanthabeca@users.noreply.github.com>
2023-11-21 15:36:44 -0300

    Add files via upload

---
+++ galeria/main.js
@@ -0,0 +1,18 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declarar um array com o nome de arquivos das imagens */
+
+/* Declarar um array com o texto declarativo para cada arquivo de imagem (o que ira aparecer no alt="") */
+
+/* Realiar um loop nas imagens */
+
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+newImage.setAttribute('alt', xxx);
+thumbBar.appendChild(newImage);
+
+/* Conectando o botão Escurecer/Clarear */
